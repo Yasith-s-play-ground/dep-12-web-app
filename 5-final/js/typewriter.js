7fa97c2f02277ac4d39db50c4d7941e79e7e8612
Yasith Ishara Perera <yasithishara@gmail.com>
2024-06-13 18:57:32 +0530

    updated final project

---
+++ 5-final/js/typewriter.js
@@ -0,0 +1,50 @@
+const displayItems = ["I'm Yasith Perera", "I'm a Software Engineer", 'Direct Entry Program 12',
+    'We', 'are', 'born to code...'];
+
+const h1Elm = document.getElementById('display-board');
+const spanElm = document.getElementById('display-span');
+const cursorElm = document.getElementById('cursor');
+
+let index = 0;
+let arrayItem;
+
+
+let letterTmr;
+
+function displayText(index) {
+    setInterval(function () {
+
+        spanElm.innerText = "";
+
+        displayWordsLetterByLetter(displayItems[index], 0);
+
+        // setTimeout(function () {
+        //     let eraseIndex = displayItems[index].length - 1;
+        //     console.log(spanElm.innerText);
+        //     let eraseTmr = setInterval(function () {
+        //         spanElm.innerText = displayItems[index].substring(0, eraseIndex);
+        //         if (eraseIndex-- === 0) clearInterval(eraseTmr);
+        //     }, 50);
+        // }, 75 * displayItems[index].length);
+
+        if (index++ === displayItems.length - 1) index = 0;
+    }, 2000);
+
+    //displayText(++index);
+}
+
+displayText(0);
+
+
+function displayWordsLetterByLetter(arrayItem, letterIndex) {
+    letterTmr = setInterval(function () {
+        spanElm.innerHTML += arrayItem.charAt(letterIndex++);
+        if (letterIndex === arrayItem.length) clearInterval(letterTmr);
+    }, 75);
+
+    return true;
+
+
+}
+
+
